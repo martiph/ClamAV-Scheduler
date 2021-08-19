3c0b399dcbb9a1b78d64b1114e5e859020ff3b56
martiph <martiph@gmx.ch>
2021-08-19 10:56:58 +0200

    added automation for installation procedure

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
+CLAM_DIR="/opt/custom_clamscan"
+CLAM_QUARANTINE="/opt/custom_clamscan/quarantine"
+CLAM_INFECTED="/var/log/clamav/infected_files.log"
+
+if [! -d $CLAM_DIR ];then
+	mkdir --parents $CLAM_DIR
+	mkdir $CLAM_QUARANTINE
+fi
+
+if [ -d $CLAM_QUARANTINE ];then
+	mkdir --parents $CLAM_QUARANTINE
+fi
+
+cp ./custom_clamscan.sh "$CLAM_DIR/"
+cp ./infected.log $CLAM_INFECTED
 cp ./ClamAV.service /etc/systemd/system/
 cp ./ClamAV.timer /etc/systemd/system/
 
-if [[! -d /opt/scripts/]];then
-	mkdir --parents /opt/scripts/
-fi
-
-cp ./periodic-clamscan.sh /opt/scripts/
-
+chmod 711 "$CLAM_DIR/custom_clamscan.sh"
